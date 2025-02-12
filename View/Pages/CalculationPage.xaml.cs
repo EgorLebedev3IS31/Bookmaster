bfa14159529be0f7d3a7f6f669afe58511df724a
Лебедев.Егор <.@TERM1>
2025-02-12 12:37:56 +0300

    Добавлены и подключены словари ресурсов

--- View/Pages/CalculationPage.xaml.cs
+++ View/Pages/CalculationPage.xaml.cs
@@ -10,19 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Bookmaster.View.Windows
+namespace Bookmaster.View.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для LoginWindow.xaml
+    /// Логика взаимодействия для Calculation_Page.xaml
     /// </summary>
-    public partial class LoginWindow : Window
+    public partial class Calculation_Page : Page
     {
-        public LoginWindow()
+        public Calculation_Page()
         {
             InitializeComponent();
         }
-
     }
 }
--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bookmaster.View.Pages;
+using Bookmaster.View.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,51 @@ namespace Bookmaster
         public MainWindow()
         {
             InitializeComponent();
+            
+            LogoutMi.Visibility=Visibility.Collapsed; LibraryMi.Visibility=Visibility.Collapsed;
+        }
+
+        private void LoginMi_Click(object sender, RoutedEventArgs e)
+        {
+            //Для реализации оконной навигиции нужно;
+            //1) Создать экземпляр окна, которое требуется открыть.
+            LoginWindow loginWindow = new LoginWindow();
+
+            //2) У экземпляра окна вызвать метод Show() или ShowDialog().
+            loginWindow.Show();
+        }
+
+        private void CloseMi_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void LogoutMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BrowseCatalogMi_Click(object sender, RoutedEventArgs e)
+        {
+            //Для реализации страничной навигации нужно:
+            //1) Обратиться к элементу Frame по имени и называем метод Navigate()
+            //2) В качестве аргумента передаем в метод экземпляр страницы, которую нужно открыть.
+            MainFrm.Navigate(new BrowseCatalogPage());
+        }
+
+        private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CirculationMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReportsMi_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
